98fb394665770e1d53ff0f308290a63e2bbf35c9
Salvador <salvadoredgar99@gmail.com>
2015-07-01 16:16:46 -0500

    Add project

---
+++ app/src/main/java/f/dev/project2_exercisefragments/DinamicFragment.java
@@ -0,0 +1,24 @@
+package f.dev.project2_exercisefragments;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by sati on 30/06/2015.
+ */
+public class DinamicFragment extends Fragment{
+
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        View viewRoot = inflater.inflate(R.layout.dinamic_fragment, container, false);
+
+        return viewRoot;
+    }
+}
